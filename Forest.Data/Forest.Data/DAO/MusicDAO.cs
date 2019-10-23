cb466c661d22cd9d55b4ece34ae72d3d84709e4c
Smith <b7040746@hallam.shu.ac.uk>
2019-10-23 10:30:20 +0100

    Forest.Data interface and class created for MusicDAO and IMusicDAO

---
+++ Forest.Data/Forest.Data/DAO/MusicDAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Forest.Data.IDAO;
+
+namespace Forest.Data.IDAO
+{
+    public class MusicDAO : IMusicDAO
+    {
+        private ForestEntities _context;
+        public MusicDAO()
+        {
+            _context = new ForestEntities();
+        }
+
+        public IList<MusicCategory> GetMusicCategory()
+        {
+            IQueryable<MusicCategory> _categories;
+            _categories = from category in _context.MusicCategory select category;
+            return _categories.ToList<MusicCategory>();
+        }
+    }
+}
