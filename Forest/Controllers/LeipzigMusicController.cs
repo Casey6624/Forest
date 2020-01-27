c034d088e5dd12fc0c36c5891930a911764cadc9
Smith <b7040746@hallam.shu.ac.uk>
2020-01-27 20:29:02 +0000

    Controller created for Leipzig on the presentation layer

---
+++ Forest/Controllers/LeipzigMusicController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Leipzig.Services.LeipzigServices;
+using Leipzig.Services.Service;
+
+namespace Forest.Controllers
+{
+    public class LeipzigMusicController : Controller
+    {
+        private Leipzig.Services.LeipzigServices.ForestWebService _remoteService;
+
+        public LeipzigMusicController()
+        {
+            _remoteService = new ForestWebService();
+        }
+        // GET: LeipzigMusic
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetLeipzigMusicCategories(int genre)
+        {
+            return View(_remoteService.GetMusicRecordings(genre));
+        }
+    }
+}
