057855ed8dcb8b2cd9949ded351bd2a5c1fb36d8
Smith <b7040746@hallam.shu.ac.uk>
2020-01-27 20:16:05 +0000

    Leipzig.Services fully implemented, now need to call from MVC project

---
+++ Leipzig.Services/Service/LeipzigMusicService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leipzig.Services.IService;
+using Leipzig.Services.Service;
+
+namespace Leipzig.Services.Service
+{
+    public class LeipzigMusicService : ILeipzigMusicService
+    {
+        private LeipzigServices.ForestWebService _proxy;
+        
+        public LeipzigMusicService()
+        {
+            _proxy = new LeipzigServices.ForestWebService();
+        }
+
+        public IList<LeipzigServices.MusicBEAN> GetLeipzigMusicRecordings(int genre)
+        {
+            return _proxy.GetMusicRecordings(genre);
+        }
+    }
+}
